da389cdd94d21b53924f2ec63008bdbfe72fc83e
Anders0nlima <andersoncon85@gmail.com>
2026-04-15 19:44:15 -0300

    .

--- Listas-Lineares/Exercicios/Exercicio_2_4/file.py
+++ Listas-Lineares/Exercicios/Exercicio_2_4/file.py
@@ -1,1 +1,14 @@
-print("-")
+"""
+Comparar o algoritmo apresentado com o Algoritmo 2.3:
+
+Em que situação o desempenho dos dois é equivalente? 
+o sentido de equivalencia está na quantidade de interações, eles se tornam equivalentes no pior caso de ambos. Eles são equivalentes na quantidade de memória acessada (os n elementos) quando a busca é forçada a ir até o final da lista.
+em casos normais como o algoritmo 2.3 possui um sentinela (faz apenas uma comparação por iteração) e o algortimo 2.4 faz duas, o 2.3 acaba ganhando em eficiencia
+
+Qual é a restrição que o algoritmo acima apresenta em relação ao Algoritmo 2.3? 
+definição de restrição: O que os meus dados precisam ter para que esse algoritmo funcione?
+A restrição do algoritmo 2.4 é que a lista precisa estar ordenada.
+O Algoritmo 2.3 (Busca Sequencial comum) funciona em qualquer lista, bagunçada ou não. Se usar o algoritmo da 2.4 em uma lista desordenada, ele pode parar no segundo elemento (se ele for maior que x) e dizer que não encontrou, mesmo que o x esteja logo depois.
+
+
+"""
--- Listas-Lineares/Exercicios/Exercicio_2_6/file.py
+++ Listas-Lineares/Exercicios/Exercicio_2_6/file.py
@@ -1,1 +1,10 @@
-print("-")
+"""
+Deque (Double-Ended Queue) é uma "Fila de Duas Pontas", (no Deque você pode inserir e remover de ambos os lados da fila - Diferente da fila comum, onde você só entra por um lado e sai pelo outro)
+
+Psedeucodigos:
+"Inserção pela Extremidade 1 (E1): Como colocar um novo elemento no "início" do vetor?"
+"Inserção pela Extremidade 2 (E2): Como colocar um novo elemento no "fim" do vetor?"
+"Remoção pela Extremidade 1 (E1): Como tirar quem está no "início"?"
+"Remoção pela Extremidade 2 (E2): Como tirar quem está no "fim"?"
+
+"""
